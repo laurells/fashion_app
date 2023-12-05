e98694ecabef1b9c39f58f749331c3db69f34974
Laurels Echichinnwo <laurels.echichinwo@stu.cu.edu.ng>
2023-12-05 20:54:20 +0000

    update

--- my-app/src/components/Auth/ForgotPassword.tsx
+++ my-app/src/components/Auth/ForgotPassword.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Dialog } from "@headlessui/react";
-import { useTranslations } from "next-intl";
 
 import { useAuth } from "../../context/AuthContext";
 import Button from "../Buttons/Button";
@@ -21,16 +20,15 @@ const ForgotPassword: React.FC<Props> = ({
 }) => {
   const auth = useAuth();
   const [email, setEmail] = useState("");
-  const t = useTranslations("LoginRegister");
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const forgotPasswordResponse = await auth.forgotPassword!(email);
     console.log(forgotPasswordResponse);
     if (forgotPasswordResponse.success) {
-      setSuccessMsg("login_successful");
+      setSuccessMsg("login successful");
     } else {
-      setErrorMsg("incorrect_email_password");
+      setErrorMsg("incorrect email password");
     }
   };
 
@@ -41,37 +39,37 @@ const ForgotPassword: React.FC<Props> = ({
          //@ts-ignore
         className="text-3xl text-center my-8 font-medium leading-10 text-gray-900"
       >
-        {t("forgot_password")}
+        {("forgot password")}
       </Dialog.Title>
       <form onSubmit={handleSubmit} className="mt-2">
         <Input
           type="email"
-          placeholder={`${t("email_address")} *`}
+          placeholder={`${("email address")} *`}
           name="email"
           required
-          extraClass="w-full focus:border-gray500"
-          border="border-2 border-gray300 mb-4"
+          extraClass="w-full focus:border-gray-500"
+          border="border-2 border-gray-300 mb-4"
           onChange={(e) => setEmail((e.target as HTMLInputElement).value)}
           value={email}
         />
         {errorMsg !== "" && (
           <div className="text-red text-sm mb-4 whitespace-nowrap">
-            {t(errorMsg)}
+            {(errorMsg)}
           </div>
         )}
         <Button
           type="submit"
-          value={t("submit")}
+          value={("submit")}
           extraClass="w-full text-center text-xl mb-4"
           size="lg"
         />
-        <div className="text-center text-gray400">
-          {t("go_back_to")}{" "}
+        <div className="text-center text-gray-400">
+          {("go back to")}{" "}
           <span
             onClick={onLogin}
-            className="text-gray500 focus:outline-none focus:underline cursor-pointer"
+            className="text-gray-500 focus:outline-none focus:underline cursor-pointer"
           >
-            {t("login")}
+            {("login")}
           </span>
         </div>
       </form>
