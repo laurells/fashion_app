78122ac3ace9569fc6379d8f81c1cafd022553bc
Laurels Echichinnwo <laurels.echichinwo@stu.cu.edu.ng>
2023-12-06 09:45:04 +0000

    changes

--- my-app/src/pages/search.tsx
+++ my-app/src/pages/search.tsx
@@ -1,10 +1,8 @@
 // import { useCallback, useEffect, useState } from "react";
 import { GetServerSideProps } from "next";
 //  GetStaticPaths, GetStaticProps
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 import Link from "next/link";
-import { useTranslations } from "next-intl";
-
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import Card from "../components/Card/Card";
@@ -19,12 +17,11 @@ type Props = {
 };
 
 const Search: React.FC<Props> = ({ items, searchWord }) => {
-  const t = useTranslations("Search");
 
   return (
     <div>
       {/* ===== Head Section ===== */}
-      <Header title={`Haru Fashion`} />
+      <Header title={`Laurels Fashion`} />
 
       <main id="main-content">
         {/* ===== Breadcrumb Section ===== */}
@@ -32,9 +29,9 @@ const Search: React.FC<Props> = ({ items, searchWord }) => {
           <div className="app-x-padding app-max-width w-full">
             <div className="breadcrumb">
               <Link href="/">
-                <a className="text-gray400">{t("home")}</a>
+                <a className="text-gray-400">{("home")}</a>
               </Link>{" "}
-              / <span>{t("search_results")}</span>
+              / <span>{("search results")}</span>
             </div>
           </div>
         </div>
@@ -42,14 +39,12 @@ const Search: React.FC<Props> = ({ items, searchWord }) => {
         {/* ===== Heading & Filter Section ===== */}
         <div className="app-x-padding app-max-width w-full mt-8">
           <h1 className="text-3xl mb-2">
-            {t("search_results")}: &quot;{searchWord}&quot;
+            {("search results")}: &quot;{searchWord}&quot;
           </h1>
           {items.length > 0 && (
             <div className="flex justify-between mt-6">
               <span>
-                {t("showing_results", {
-                  products: items.length,
-                })}
+                {`showing results Products: ${items.length}`}
               </span>
             </div>
           )}
@@ -59,7 +54,7 @@ const Search: React.FC<Props> = ({ items, searchWord }) => {
         <div className="app-x-padding app-max-width mt-3 mb-14">
           {items.length < 1 ? (
             <div className="flex justify-center items-center h-72">
-              {t("no_result")}
+              {("no result")}
             </div>
           ) : (
             <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-x-4 gap-y-10 sm:gap-y-6 mb-10">
