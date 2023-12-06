e920c8eb42c3bb57a534d348daca88e068554eb8
Laurels Echichinnwo <laurels.echichinwo@stu.cu.edu.ng>
2023-12-06 10:04:20 +0000

    changes

--- my-app/public/icons/Loading.tsx
+++ my-app/public/icons/Loading.tsx
@@ -1,11 +1,8 @@
-import styles from "./Loading.module.css";
-
 const Loading = () => (
-  <div className={styles.ldsEllipsis}>
-    <div></div>
-    <div></div>
-    {/* <div></div> */}
-    <div></div>
+  <div className="relative w-10 h-10 mx-auto mt-8">
+    <div className="absolute top-2 left-2 w-2 h-2 rounded-full bg-gray-700 animate-ldsEllipsis1"></div>
+    <div className="absolute top-2 left-6 w-2 h-2 rounded-full bg-gray-700 animate-ldsEllipsis2"></div>
+    <div className="absolute top-2 left-10 w-2 h-2 rounded-full bg-gray-700 animate-ldsEllipsis3"></div>
   </div>
 );
 
